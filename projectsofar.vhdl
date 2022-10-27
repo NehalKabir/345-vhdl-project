047462ea5a3c605e734b0c04fbf383c28965d278
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2022-10-27 11:31:14 -0400

    Update projectsofar.vhdl

--- projectsofar.vhdl
+++ projectsofar.vhdl
@@ -41,14 +41,16 @@ signal sel : std_logic_vector (24 downto 0);
 signal r : std_logic_vector(1 downto 0);  
 signal op : std_logic_vector(2 downto 0);
 begin  
-	process 
+	process(reg1, reg2, reg3)
+	variable temp : std_logic_vector (127 downto 0);
+	variable temp2 : std_logic_vector (127 downto 0);
 	begin 					 
 	op <= sel(22 downto 20);
 		--r4 instructions 
 	if sel(24 downto 23) = "10"	then 
 		--mult add low--
 		if sel(22 downto 20) = "000"	 then   
-		
+			
 		--mult add high--
 		elsif sel(22 downto 20) = "001"	then 
 		
@@ -60,7 +62,12 @@ begin
 			
 		--long int mult add low
 		elsif sel(22 downto 20) = "100" then 
-			
+			temp(63 downto 0) := std_logic_vector(resize(signed(reg2(31 downto 0)) * signed(reg3(31 downto 0)), 64));
+			temp(127 downto 64) := std_logic_vector(resize(signed(reg2(95 downto 64)) * signed(reg3(95 downto 64)), 64));
+			temp2(63 downto 0) := std_logic_vector(signed(reg1(63 downto 0)) + signed(temp(63 downto 0)));
+			temp2(127 downto 64) := std_logic_vector(signed(reg1(127 downto 64)) + signed(temp(127 downto 64)));
+			output <= temp2;
+						
 		--long mult add high
 		elsif sel(22 downto 20) = "101" then		
 			
@@ -76,62 +83,63 @@ begin
 	 ---- r3 instructions--
 	 elsif sel(24 downto 23) = "11" then 
 		 --nop
-		 if sel( 22 downto 15) = "----0000"	 then 
+		 if sel( 18 downto 15) = "0000"	 then 
 			 
 		--count leading 0 in words--
-		elsif sel( 22 downto 15) = "------10" then 
+		elsif sel( 18 downto 15) = "0010" then 
 		
 		--add word unsigned
-		elsif sel( 22 downto 15) = "----0011" then 
+		elsif sel( 18 downto 15) = "0011" then 
 		
 		--add halfwors unsigned 
-		elsif sel( 22 downto 15) = "----0100" then
+		elsif sel( 18 downto 15) = "0100" then
 			
 		--bitwise logic and
-		elsif sel( 22 downto 15) = "----0101" then
+		elsif sel( 18 downto 15) = "0101" then
 		
 			--broadcast word
-		elsif sel( 22 downto 15) = "----0110" then		
+		elsif sel( 18 downto 15) = "0110" then		
 		
 		--max signed word
-		elsif sel( 22 downto 15) = "----0111" then 
+		elsif sel( 18 downto 15) = "0111" then 
 			
 		--min signed word
-		elsif sel( 22 downto 15) = "----1000" then 
+		elsif sel( 18 downto 15) = "1000" then 
 		
 		--mult low unsigned 
-		elsif sel( 22 downto 15) = "----1001" then 
+		elsif sel( 18 downto 15) = "1001" then 
 		
 		--mult low by constant unsigned
-		elsif sel( 22 downto 15) = "----1010" then 
+		elsif sel( 18 downto 15) = "1010" then 
 		
 		--OR
-		elsif sel( 22 downto 15) = "----1011" then 
+		elsif sel( 18 downto 15) = "1011" then 
 			
 		--count ones in words
-		elsif sel( 22 downto 15) = "----1100" then 
+		elsif sel( 18 downto 15) = "1100" then 
 		
 		--rotate bits in words
-		elsif sel( 22 downto 15) = "----1101" then 
+		elsif sel( 18 downto 15) = "1101" then 
 			
 		--sub from word unsiged 
-		elsif sel( 22 downto 15) = "----1110" then 
+		elsif sel( 18 downto 15) = "1110" then 
 			
 		--sub from halfwarod saturated
-		elsif sel( 22 downto 15) = "----1111" then 
+		elsif sel( 18 downto 15) = "1111" then 
 			
 		 
 		end if; --r3 end  
 		
 	--load immediate
-	elsif sel (24) = "0" then
+	elsif sel (24) = '0' then
 		
 		end if;
-		 
-	 end if;
 	 
 	  end process;
 
 end alu;
+	
+
+
 	
 
