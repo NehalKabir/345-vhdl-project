a754e1a986018ae61f971a354f6dab901e14af78
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2022-11-13 15:30:37 -0500

    Add files via upload

---
+++ instruction_buffer.vhdl
@@ -0,0 +1,39 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : instruction_buffer
+-- Design      : project_part1
+-- Author      : thomas plourde
+-- Company     : HP Inc.
+--
+-------------------------------------------------------------------------------
+--
+-- File        : E:\ESE 345\Project_attempt2\project_part1\src\instruction_buffer.vhd
+-- Generated   : Sun Nov 13 15:16:38 2022
+-- From        : interface description file
+-- By          : Itf2Vhdl ver. 1.22
+--
+-------------------------------------------------------------------------------
+--
+-- Description : 
+--
+-------------------------------------------------------------------------------
+
+--{{ Section below this comment is automatically maintained
+--   and may be overwritten
+--{entity {instruction_buffer} architecture {instruction_buffer}}
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity instruction_buffer is
+end instruction_buffer;
+
+--}} End of automatically maintained section
+
+architecture instruction_buffer of instruction_buffer is
+begin
+
+	 -- enter your statements here --
+
+end instruction_buffer;
