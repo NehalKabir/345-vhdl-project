acae4469e0be6ecf3b062110798e0fb581739d7f
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2022-11-26 15:23:51 -0500

    Update TB_readFILE_SIGNALS.vhdl

--- TB_readFILE_SIGNALS.vhdl
+++ TB_readFILE_SIGNALS.vhdl
@@ -23,17 +23,10 @@
 --{entity {tstbnch} architecture {tstbnch}}
 
 library IEEE;
-
-package custom_types is
-    use IEEE.std_logic_1164.all;	
---	type std_logic_aoa is array (0 to 31) of std_logic_vector(127 downto 0);
-    type a is array (0  to 63) of std_logic_vector (24 downto 0);
-end package;
-
-library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use work.all;		
+use work.custom_types.all;
+use work.all;
 use std.textio.all;
 
 entity tstbnch is
@@ -42,37 +35,34 @@ end tstbnch;
 --}} End of automatically maintained section
 
 architecture tstbnch of tstbnch is
+--basic
 signal clk: std_logic;
-signal PC: integer;
-type a is array (0  to 63) of std_logic_vector (24 downto 0);	  
+signal PC: integer;	  
 signal instruction : a;	 
 type b is array ( 0 to 32) of std_logic_vector( 127 downto 0);
 signal reg_data : b;   			 
---alu
-signal reg1: std_logic_vector (127 downto 0 );
-signal reg2: std_logic_vector (127 downto 0 );
-signal reg3: std_logic_vector (127 downto 0 );
-signal output_alu: std_logic_vector (127 downto 0 );	
---reg file
-signal write_reg: std_logic_vector (127 downto 0 );	 
-signal sel : std_logic_vector (24 downto 0);
-signal write : std_logic; 
-signal output1: std_logic_vector (127 downto 0 );	
-signal output2: std_logic_vector (127 downto 0 );	
-signal output3: std_logic_vector (127 downto 0 );	
---ins buffer	
-signal reg : a;
-signal output_ins: std_logic_vector (24 downto 0);
---for unit
-signal curr_instr: std_logic_vector(24 downto 0);
-signal	prev_instr: std_logic_vector(24 downto 0);
+
+signal i_if_d: std_logic_vector(24 downto 0);
+signal i_id_ex1: std_logic_vector(127 downto 0);
+signal i_id_ex2: std_logic_vector(127 downto 0);
+signal i_id_ex3: std_logic_vector(127 downto 0);
+signal i_id_ex4: integer;
+signal i_ex_wb1: std_logic_vector(127 downto 0);
+signal i_ex_wb2: integer;
 	
-signal	slct:  std_logic ;
-
-
-
+	
+signal o_if_d: std_logic_vector(24 downto 0);
+signal o_id_ex1: std_logic_vector(127 downto 0);
+signal o_id_ex2: std_logic_vector(127 downto 0);
+signal o_id_ex3: std_logic_vector(127 downto 0);
+signal o_id_ex4: integer;
+signal o_ex_wb1: std_logic_vector(127 downto 0);
+signal o_ex_wb2: integer;
+-- buff
+signal prev_ins: std_logic_vector(24 downto 0);
 begin
-  process is
+	
+	process is
   variable current_line : line;  
   			 --ins 64 25 bit
         variable current_line_reg : line;	 
@@ -108,5 +98,39 @@ begin
         end loop;
         file_close(reg_file);
 	 end process;
+	
+	UUT1 : entity instruction_buffer
+		port map (
+		clk => clk,
+		PC => PC,
+		reg => instruction,
+		output => i_if_d,
+		prev_output => prev_ins
+		);
+	
+	UUT2 : entity reg_file
+		port map (
+		clk => clk,
+		write_reg => o_ex_wb1,
+		sel => o_if_d,
+		output1 => i_id_ex1,
+		output2 => i_id_ex2,
+		output3 => i_id_ex3,
+		write => o_ex_wb2,
+		write_o => i_id_ex4
+		);
+	
+	-- System Clock Process
+	clock_gen : process
+	begin
+		clk <= '0';
+		wait for clk_period/2;
+		loop	-- inifinite loop
+			clk <= not clk;
+			wait for clk_period/2;
+			exit when END_SIM = true;
+		end loop;
+		std.env.finish;
+	end process;
 
 end tstbnch;
