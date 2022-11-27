1d8ba63b11e15cfc9ea008532cc75ddcbc894c25
NehalKabir <92537145+NehalKabir@users.noreply.github.com>
2022-11-26 20:35:11 -0500

    Update Mips.asm

--- Mips.asm
+++ Mips.asm
@@ -1,2 +1,25 @@
+LI 1 2 1
 SIMALS 1 2 3 4
 SIMAHS 4 3 2 1
+SIMSLS 9 3 6 8
+SIMSHS 10 3 11 12
+SLIMALS 17 16 12 2
+SLIMAHS 8 5 14 3
+SLIMSLS 20 22 21 24
+SLIMSHS 29 8 27 23
+nop 14 19 22
+clzw 30 27 15
+au 5 4 6 7
+ahu 9 8 7
+ahs 10 11 12
+and 1 2 3
+bcw 2 5 13
+maxws 18 28 8
+minws 16 20 21
+mlhu 9 4 9
+mlhcu 6 15 14
+or 4 5 6
+pcntw 18 23 24
+rotw 4 9 10 
+sfwu 19 16 13
+sfhs 25 26 27
